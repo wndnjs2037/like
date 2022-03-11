2bf9bc412fa0ba2227f8d6c94b5183af379a36bb
Juwon <71490091+wndnjs2037@users.noreply.github.com>
2022-03-11 11:42:35 +0900

    Update schema.sql
    소셜피드 수정

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -33,42 +33,6 @@ CREATE TABLE comment
     FOREIGN KEY(post_id) REFERENCES post(id)
 );
 
--- 소셜피드 테이블 
-CREATE TABLE social_feed 
-(
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    author BIGINT NOT NULL, -- users table (user_id)
-    createAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    post_id BIGINT NOT NULL, -- post table 
-    other_id BIGINT NOT NULL, -- follow table 
-    PRIMARY KEY(id),
-    FOREIGN KEY(author) REFERENCES users(id),
-    FOREIGN KEY(post_id) REFERENCES post(id)
-    -- FOREIGN KEY(other_id) REFERENCES follow(id)  -> follow 테이블 생성 후 주석 제거 예정
-);
-
--- 친구태그 테이블
-CREATE TABLE friend_tag 
-(
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    tag_name VARCHAR(255) NOT NULL,
-    user_id BIGINT NOT NULL, -- users table (id)
-    post_id BIGINT NOT NULL, -- post table 
-    PRIMARY KEY(id),
-    FOREIGN KEY(user_id) REFERENCES users(id),
-    FOREIGN KEY(post_id) REFERENCES post(id)
-);
-
--- 해시태그 테이블
-create table hash_tag (
-    id BIGINT not null AUTO_INCREMENT,
-    tag_content VARCHAR(255) NOT NULL, 
-    author BIGINT NOT NULL, -- users table (id)
-    post_id BIGINT NOT NULL, -- post table
-    PRIMARY KEY(id),
-    FOREIGN KEY(author) REFERENCES users(id),
-    FOREIGN KEY(post_id) REFERENCES post(id)
-);
 
 -- 팔로우
 CREATE TABLE follow(
@@ -95,4 +59,41 @@ CREATE TABLE likes(
 	article_id BIGINT NOT NULL,
 	FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE,
 	FOREIGN KEY(article_id) REFERENCES post(id) ON DELETE CASCADE
+);
+
+-- 소셜피드 테이블 
+CREATE TABLE social_feed 
+(
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    author BIGINT NOT NULL, -- users table (user_id)
+    createAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    post_id BIGINT NOT NULL, -- post table 
+    follow_id BIGINT NOT NULL, -- follow table 
+    PRIMARY KEY(id),
+    FOREIGN KEY(author) REFERENCES users(id),
+    FOREIGN KEY(post_id) REFERENCES post(id),
+    FOREIGN KEY(follow_id) REFERENCES follow(id)
+);
+
+-- 친구태그 테이블
+CREATE TABLE friend_tag 
+(
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    tag_name VARCHAR(255) NOT NULL,
+    user_id BIGINT NOT NULL, -- users table (id)
+    post_id BIGINT NOT NULL, -- post table 
+    PRIMARY KEY(id),
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(post_id) REFERENCES post(id)
+);
+
+-- 해시태그 테이블
+create table hash_tag (
+    id BIGINT not null AUTO_INCREMENT,
+    tag_content VARCHAR(255) NOT NULL, 
+    author BIGINT NOT NULL, -- users table (id)
+    post_id BIGINT NOT NULL, -- post table
+    PRIMARY KEY(id),
+    FOREIGN KEY(author) REFERENCES users(id),
+    FOREIGN KEY(post_id) REFERENCES post(id)
 );
